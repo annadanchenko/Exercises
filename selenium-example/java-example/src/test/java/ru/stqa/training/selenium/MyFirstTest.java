604ed6119fffae07472670a9b7501391d01c6f32
annadanchenko <anna.danchenko@gmail.com>
2017-01-26 20:09:36 +0100

    LoginTest added
    Added test for login to http://localhost/litecart/admin. Removed test
    myFirstTest for google page.

--- selenium-example/java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ selenium-example/java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -3,10 +3,13 @@ package ru.stqa.training.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.NoSuchElementException;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest {
@@ -15,18 +18,24 @@ public class MyFirstTest {
 
     @Before
     public void start() {
-
             driver = new ChromeDriver();
             wait = new WebDriverWait(driver, 10);
+    }
 
-    }
     @Test
-    public void myFirstTest() {
-        driver.get("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnG")).click();
-        wait.until(titleIs("webdriver - Google Search"));
+    public void loginTest() {
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        try{
+            WebElement sidebar = driver.findElement(By.id("widget-sales"));
+        } catch (NoSuchElementException  e) {
+            Assert.fail("widget-sales not found on page after login attempt");
+        }
     }
+
 
     @After
     public void stop() {
