0810fee83e743e005757b0c99bc47ba90319d6de
annadanchenko <anna.danchenko@gmail.com>
2017-02-07 17:23:21 +0100

    Changed Exersise 7 (use loops)

--- selenium-example/java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ selenium-example/java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -7,7 +7,10 @@ import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import java.util.Iterator;
+import java.util.List;
+import java.lang.String.*;
+import java.util.ArrayList;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -39,7 +42,7 @@ public class MyFirstTest {
         // driver = new FirefoxDriver(bin, new FirefoxProfile());
         //  driver = new InternetExplorerDriver();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @Test
@@ -49,89 +52,19 @@ public class MyFirstTest {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
 
-        try{
-            //Appearence
-            checkItem(By.cssSelector("ul#box-apps-menu li:first-child"));
-            checkItem(By.id("doc-template"));
-            checkItem(By.id("doc-logotype"));
-            //Catalog
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(2) span.icon-wrapper"));
-            checkItem(By.id("doc-catalog"));
-            checkItem(By.id("doc-product_groups"));
-            checkItem(By.id("doc-option_groups"));
-            checkItem(By.id("doc-manufacturers"));
-            checkItem(By.id("doc-suppliers"));
-            checkItem(By.id("doc-delivery_statuses"));
-            checkItem(By.id("doc-sold_out_statuses"));
-            checkItem(By.id("doc-quantity_units"));
-            checkItem(By.id("doc-csv"));
-            //Countries
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(3) span.icon-wrapper"));
-            //Currencies
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(4) span.icon-wrapper"));
-            //Customers
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(5) span.icon-wrapper"));
-            checkItem(By.id("doc-customers"));
-            checkItem(By.id("doc-csv"));
-            checkItem(By.id("doc-newsletter"));
-            //Geo Zones
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(6) span.icon-wrapper"));
-            //Languages
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(7) span.icon-wrapper"));
-            checkItem(By.id("doc-languages"));
-            checkItem(By.id("doc-storage_encoding"));
-            //Modules
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(8) span.icon-wrapper"));
-            checkItem(By.id("doc-jobs"));
-            checkItem(By.id("doc-customer"));
-            checkItem(By.id("doc-shipping"));
-            checkItem(By.id("doc-payment"));
-            checkItem(By.id("doc-order_total"));
-            checkItem(By.id("doc-order_success"));
-            checkItem(By.id("doc-order_action"));
-            //Orders
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(9) span.icon-wrapper"));
-            checkItem(By.id("doc-orders"));
-            checkItem(By.id("doc-order_statuses"));
-            //Pages
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(10) span.icon-wrapper"));
-            //Reports
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(11) span.icon-wrapper"));
-            checkItem(By.id("doc-monthly_sales"));
-            checkItem(By.id("doc-most_sold_products"));
-            checkItem(By.id("doc-most_shopping_customers"));
-            //Settings
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(12) span.icon-wrapper"));
-            checkItem(By.id("doc-store_info"));
-            checkItem(By.id("doc-defaults"));
-            checkItem(By.id("doc-general"));
-            checkItem(By.id("doc-listings"));
-            checkItem(By.id("doc-images"));
-            checkItem(By.id("doc-checkout"));
-            checkItem(By.id("doc-advanced"));
-            checkItem(By.id("doc-security"));
-            //Slides
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(13) span.icon-wrapper"));
-            //Tax
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(14) span.icon-wrapper"));
-            checkItem(By.id("doc-tax_classes"));
-            checkItem(By.id("doc-tax_rates"));
-            //Translations
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(15) span.icon-wrapper"));
-            checkItem(By.id("doc-search"));
-            checkItem(By.id("doc-scan"));
-            checkItem(By.id("doc-csv"));
-            //Users
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(16) span.icon-wrapper"));
-            //vQmods
-            checkItem(By.cssSelector("ul#box-apps-menu li:nth-child(17) span.icon-wrapper"));
-            checkItem(By.id("doc-vqmods"));
-
-        } catch (NoSuchElementException  e) {
-            Assert.fail("element not found");
+        List<WebElement> mainItems = driver.findElements(By.cssSelector("ul#box-apps-menu span.icon-wrapper"));
+        for (int i = 0; i < mainItems.size(); i++) {
+            String mainItemNumber = Integer.toString(i+1);
+            String mainLocator = String.format("ul#box-apps-menu li:nth-child(%s) span.icon-wrapper",mainItemNumber);
+            checkItem(By.cssSelector(mainLocator));
+            List<WebElement> subItems = driver.findElements(By.cssSelector("ul.docs li"));
+            for (int j = 0; j < subItems.size(); j++) {
+                String subItemNumber = Integer.toString(j+1);
+                String subLocator = String.format("ul.docs li:nth-child(%s)",subItemNumber);
+                checkItem(By.cssSelector(subLocator));
+            }
         }
     }
-
 
     @After
     public void stop() {
