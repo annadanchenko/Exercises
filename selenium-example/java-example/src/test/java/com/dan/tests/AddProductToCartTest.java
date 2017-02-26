55a0c7827a2e281afe566c23ddfdb08777facce9
annadanchenko <anna.danchenko@gmail.com>
2017-02-26 16:10:30 +0100

    Ex 13. Changed

--- selenium-example/java-example/src/test/java/com/dan/tests/AddProductToCartTest.java
+++ selenium-example/java-example/src/test/java/com/dan/tests/AddProductToCartTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidSelectorException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,6 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class AddProductToCartTest {
@@ -20,6 +22,14 @@ public class AddProductToCartTest {
     private void cleanElementAndSetValue(WebElement element, String value){
         element.clear();
         element.sendKeys(value);
+    }
+
+    public boolean isElementPresent(By locator) {
+        try {
+            return driver.findElements(locator).size() > 0;
+        } catch (InvalidSelectorException ex) {
+            throw ex;
+        }
     }
 
     @Before
@@ -41,14 +51,15 @@ public class AddProductToCartTest {
         driver.get("http://localhost/litecart/en/");
 
         // 2) открыть первый товар из списка
-        WebElement productHome = driver.findElement(By.cssSelector("div#box-campaigns li.product:first-child"));
+        WebElement productHome = driver.findElement(By.cssSelector("div#box-most-popular li.product:first-child"));
         productHome.click();
 
         //2) добавить его в корзину (при этом может случайно добавиться товар, который там уже есть, ничего страшного)
-        WebElement selectSize = driver.findElement(By.name("options[Size]"));
-        Select dropdown = new Select(selectSize);
-        dropdown.selectByVisibleText("Small");
-
+       if (isElementPresent(By.name("options[Size]"))) {
+           WebElement selectSize = driver.findElement(By.name("options[Size]"));
+           Select dropdown = new Select(selectSize);
+           dropdown.selectByVisibleText("Small");
+       }
         WebElement txtQuantityInCart = driver.findElement(By.cssSelector("span.quantity"));
         String quantityBeforeAddingProduct = txtQuantityInCart.getText();
 
@@ -64,32 +75,30 @@ public class AddProductToCartTest {
     @Test
     public void addProductTest() {
         // 4) вернуться на главную страницу, повторить предыдущие шаги ещё два раза, чтобы в общей сложности в корзине было 3 единицы товара
-        for (int i = 0; i<=2; i++)
+        for (int i = 0; i<=2; i++) {
             addProduct();
-
+        }
         // 5) открыть корзину (в правом верхнем углу кликнуть по ссылке Checkout)
         driver.findElement(By.cssSelector("div#cart a.link")).click();
 
         // 6) удалить все товары из корзины один за другим, после каждого удаления подождать, пока внизу обновится таблица
-        WebElement counter =  driver.findElement(By.cssSelector("input[name='quantity']"));
-        String currentProductsQuantityInCart = counter.getAttribute("value");
-        int changedQuantity = Integer.parseInt(currentProductsQuantityInCart) - 1;
+        //number of product rows in Order Summary table
+        int numberOfProducts =  driver.findElements(By.cssSelector("td.item")).size();
 
-        while (changedQuantity >= 0) {
-            String newProductsQuantityInCart = Integer.toString(changedQuantity);
-            //decrease number of products
-            counter =  driver.findElement(By.cssSelector("input[name='quantity']"));
-            cleanElementAndSetValue(counter, newProductsQuantityInCart);
-            driver.findElement(By.name("update_cart_item")).click();
+        WebElement product  = null;
+        WebElement btnDelete = null;
 
-             //wait for table to be updated with new number of products or for "No products" msg if 0 products
-             if (changedQuantity > 0) {
-                 wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("table.dataTable tr:nth-child(2) td:nth-child(1)"), newProductsQuantityInCart));
-             }
-             else {
-               wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("div#checkout-cart-wrapper em"), "There are no items in your cart."));
-             }
-             changedQuantity--;
+        while (isElementPresent(By.cssSelector("li.shortcut:nth-child(1)"))) {
+            product = driver.findElement(By.cssSelector("li.shortcut:nth-child(1)"));
+            product.click();
+            btnDelete = driver.findElement(By.name("remove_cart_item"));
+            btnDelete.click();
+            wait.until(ExpectedConditions.numberOfElementsToBeLessThan(By.cssSelector("td.item"), numberOfProducts));
+            numberOfProducts = driver.findElements(By.cssSelector("td.item")).size();
         }
+        //remove last product
+        btnDelete = driver.findElement(By.name("remove_cart_item"));
+        btnDelete.click();
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("div#checkout-cart-wrapper em"), "There are no items in your cart."));
     }
 }
